9c7caeb79cc512ff33d43603dcac4601376126cf
Petr Peller <petr.peller@gmail.com>
2018-01-09 20:28:43 +0100

    initial implementation

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-import glob from 'glob'
-import xcode from 'xcode'
+var fs = require('fs');
+var path = require('path');
+var glob = require('glob');
+var xcode = require('xcode');
 
-glob("../*", {}, (files) => {
-  console.log(files);
+
+glob('node_modules/**/ios/**/*.xcodeproj/project.pbxproj', {}, function(err, files) {
+  files.map(function(file){
+    var xcodeProject = xcode.project(file);
+    xcodeProject.parse(function(){
+      console.log('Adding headers to ' + file);
+      var podsPath = path.resolve(__dirname, 'ios/Pods/Headers/Public');
+      xcodeProject.addToHeaderSearchPaths(podsPath);
+      fs.writeFileSync(file, xcodeProject.writeSync());
+    });
+  });
 });
