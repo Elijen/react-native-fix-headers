026705a605e930064acf3a693e7829669d947e28
Petr Peller <petr.peller@gmail.com>
2018-01-10 00:38:39 +0100

    Fancy colors and npm bin

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ glob('node_modules/**/ios/**/*.xcodeproj/project.pbxproj', {}, function(err, fil
   files.map(function(file){
     var xcodeProject = xcode.project(file);
     xcodeProject.parse(function(){
-      console.log('Adding headers to ' + file);
-      var podsPath = path.resolve(__dirname, 'ios/Pods/Headers/Public');
+      console.log('Adding headers to', '\x1b[36m' + file + '\x1b[0m');
+      var podsPath = '"' + path.resolve(__dirname, '../../ios/Pods/Headers/Public/**') + '"';
       xcodeProject.addToHeaderSearchPaths(podsPath);
       fs.writeFileSync(file, xcodeProject.writeSync());
     });
