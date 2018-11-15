965d567bbe3330cf9862d1cc6a56488f874e9bbb
Petr Peller <petr.peller@gmail.com>
2018-11-15 17:05:48 +0100

    Prettier 💅

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var fs = require('fs');
-var path = require('path');
-var glob = require('glob');
-var xcode = require('xcode');
+var fs = require("fs");
+var path = require("path");
+var glob = require("glob");
+var xcode = require("xcode");
 
-
-glob('node_modules/**/*.xcodeproj/project.pbxproj', {}, function(err, files) {
-  files.map(function(file){
+glob("node_modules/**/*.xcodeproj/project.pbxproj", {}, function(err, files) {
+  files.map(function(file) {
     var xcodeProject = xcode.project(file);
-    xcodeProject.parse(function(){
-      console.log('Adding headers to', '\x1b[36m' + file + '\x1b[0m');
-      var podsPath = '"' + path.resolve(__dirname, '../../ios/Pods/Headers/Public/**') + '"';
+    xcodeProject.parse(function() {
+      console.log("Adding headers to", "\x1b[36m" + file + "\x1b[0m");
+      var podsPath =
+        '"' + path.resolve(__dirname, "../../ios/Pods/Headers/Public/**") + '"';
       xcodeProject.addToHeaderSearchPaths(podsPath);
       fs.writeFileSync(file, xcodeProject.writeSync());
     });
